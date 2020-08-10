7c870783243fb7d0ac819734e2edb9c110fdc911
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-08-10 16:33:23 -0700

    style changes

--- haiven-web/src/index.js
+++ haiven-web/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Header />
+    <Footer />
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
