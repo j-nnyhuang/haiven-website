2fa6eff5546aff51c013b4b2ce788ae91b543adc
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-08-13 23:47:05 -0700

    Updates

---
+++ precache-manifest.58f553607985b73ddcd51fa1f2cfff2d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4994b43b9983e63ca78b78ce559eb377",
+    "url": "/haiven-website/index.html"
+  },
+  {
+    "revision": "9e7ba1be6a22ef156757",
+    "url": "/haiven-website/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "75fa00c2766ad4ddd1ad",
+    "url": "/haiven-website/static/css/main.0ec36d83.chunk.css"
+  },
+  {
+    "revision": "9e7ba1be6a22ef156757",
+    "url": "/haiven-website/static/js/2.1131f7d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/haiven-website/static/js/2.1131f7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fa00c2766ad4ddd1ad",
+    "url": "/haiven-website/static/js/main.3a9a3d91.chunk.js"
+  },
+  {
+    "revision": "80311814b7eb6a39eb5f",
+    "url": "/haiven-website/static/js/runtime-main.48e6fff0.js"
+  },
+  {
+    "revision": "06668fbf3334cea3d2b742c7c1fd6e99",
+    "url": "/haiven-website/static/media/Prototype.06668fbf.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/haiven-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "967dc2352a1495e383519329dc2abb16",
+    "url": "/haiven-website/static/media/undraw_alert.967dc235.svg"
+  },
+  {
+    "revision": "a440ed89b36eb8248e5347d17adc99e3",
+    "url": "/haiven-website/static/media/undraw_calling.a440ed89.svg"
+  },
+  {
+    "revision": "abcfed666cd145f84d1a4aee84a018b4",
+    "url": "/haiven-website/static/media/undraw_safe.abcfed66.svg"
+  }
+]);
